e0dbea4a86ff05038fa9cd973eaedead5dc4b4d0
Benson Njihia <bensonnjihia42@gmail.com>
2020-08-21 15:17:43 +0300

    added a list to the string with names of male children

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
